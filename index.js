bf666acf28e23b215987e0938d4c274a9c801ddc
Anton Toropanov <tanotify@gmail.com>
2024-03-16 20:08:32 +0300

    fix(index.js): update DIARY_PATH to use relative path for better portability feat(index.js): refactor code to use async/await and promisify fs functions for better readability and error handling feat(index.js): add delay function to introduce delay in processing for better performance feat(index.js): add support for reading and updating metadata in markdown files feat(package.json): add parse-markdown-metadata dependency for parsing markdown metadata

--- index.js
+++ index.js
@@ -1,48 +1,88 @@
 import fs from 'fs';
+import util from 'util';
+// import { exec } from "child_process";
+import {exec} from "node:child_process";
 
 const TABLE_HABIT_BACKUP_PATH = 'data.json';
-const DIARY_PATH = '~/Yandex.Disk.localized/Obsidian/Diary/Data';
+const DIARY_PATH = '../../Yandex.Disk.localized/Obsidian/Diary/Data';
 
-try {
-  const jsonData = fs.readFileSync(TABLE_HABIT_BACKUP_PATH, 'utf8');
-  const { habits } = JSON.parse(jsonData);
+const jsonData = fs.readFileSync(TABLE_HABIT_BACKUP_PATH, 'utf8');
+const { habits } = JSON.parse(jsonData);
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
+const existsSync = util.promisify(fs.existsSync);
+const execPromise = util.promisify(exec);
 
-  habits.forEach(({ records, name }) => {
-    records.forEach(({ create_t, record_value }) => {
-      const date = new Date(create_t * 1000);
-      const day = String(date.getDate() + 1).padStart(2, '0');
-      const month = String(date.getMonth() + 1).padStart(2, '0');
-      const year = date.getFullYear();
-      
-      const relativePath = `${year}/${month}/${day}.md`;
-      // const absolutePath = `${DIARY_PATH}/${relativePath}`;
-      const absolutePath = '/Users/tanotify/Yandex.Disk.localized/Obsidian/Diary/Data/2024/03/15.md';
+function delay(time) {
+  return new Promise(resolve => setTimeout(resolve, time));
+} 
 
-      const isCompleted = record_value === 1 ? true : false;
-
-      fs.readFile(absolutePath, 'utf8', (err, content) => {
-        let data = content || '';
-    
-        if (!data.startsWith('---')) {
-          data = '---\n\n---\n' + data;
-        }
-
-        const regex = new RegExp(`---.*${name}.*---`);
-
-        if (!regex.test(data)) {
-          console.log({ name, data })
-          data = data.replace(/^---/, `$&\n${name}: ${isCompleted}`);
-        }
-      
-        fs.writeFile(absolutePath, data, 'utf8', err => {
-          if (err) return;
-          // console.log('Written successfully!')
-        });
-      });
-      // console.log({ relativePath, date, isCompleted });
-    });
-  });
-  // console.log(data);
-} catch (err) {
-  console.error('Error reading file:', err);
+async function readFileIfExists(filename) {
+  try {
+    return await readFile(filename, 'utf8');
+  } catch (error) {
+    console.log({ filename, error })
+    return await '';
+  }
 }
+
+habits.forEach(async ({ records, name }) => {
+  await delay(500);
+  for (const { create_t, record_value: recordValue, record_type: recordType } of records) {
+    await delay(500);
+    const date = new Date(create_t * 1000);
+    const day = String(date.getDate() + 1).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = date.getFullYear();
+
+    const relativePath = `${year}/${month}/${day}.md`;
+    const absolutePath = `${DIARY_PATH}/${relativePath}`;
+    // const absolutePath = '/Users/tanotify/Yandex.Disk.localized/Obsidian/Diary/Data/2024/03/13.md';
+
+    const record = recordType && [0, 1, 2].includes(recordType) ? Boolean(recordValue) : recordValue;
+
+    try {
+      await delay(500);
+      const data = await readFileIfExists(absolutePath);
+
+      const regex = /^---(.*?)^---/gms;
+      const properties = Array.from(data.matchAll(regex), m => m[1].trim().split('\n'))[0]?.reduce((prev, line) => {
+        const [key, value] = line.split(': ');
+        return { ...prev, [key]: value };
+      }, {}) || {};
+
+      const parts = data.split('---');
+      const lastIndex = parts.length - 1;
+      const text = parts[lastIndex].trim();
+      console.log({ text });
+
+      properties[name] = record;
+      console.log({ properties, text });
+
+      
+      let updatedContent = '';
+      console.log('START UPDATE', updatedContent)
+      const propertyKeys = Object.keys(properties);
+      if (propertyKeys.length > 0) {
+        updatedContent += '---\n';
+        propertyKeys.forEach(property => {
+          updatedContent += `${property}: ${properties[property]}\n`;
+        });
+        updatedContent += '---\n';
+      }
+      updatedContent += text.trim();
+
+      console.log('BEFORE UPDATE', updatedContent)
+
+      await fs.writeFileSync(absolutePath, updatedContent, { mode: '644' })
+
+      // const stream = fs.createWriteStream(absolutePath, { mode: '0o644' });
+      // stream.write(updatedContent);
+      // stream.end();
+      // console.log('AFTER', { updatedContent, properties });
+      await delay(500);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+});
